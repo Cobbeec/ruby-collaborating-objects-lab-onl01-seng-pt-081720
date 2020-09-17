7ee4d757036239f85822d6ca9a735d6e8d2f5a6a
Elizabeth Cobb <cobbeec@gmail.com>
2020-09-17 15:24:46 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -12,7 +12,8 @@ class MP3Importer
     
     def import 
       self.files each do |file| 
-       song =   Song.new_by_filename(file)
+       song = Song.new_by_filename(file)
+       
       end 
     end 
     
